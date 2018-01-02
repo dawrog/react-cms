629e560c67acfa5db4be411621343b9e9cb48965
dawrog <dawid.rogal@protonmail.com>
2018-01-02 23:54:28 +0100

    added functionality for projects number

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -37,14 +37,21 @@ export default class Projects extends Component {
 		this.state = {serverData: {}}
 	}
 	componentDidMount() {
-		this.setState({serverData: fakeServerData})
+		setTimeout(() =>{
+			this.setState({serverData: fakeServerData})
+		}, 1000)
 	}
 	render() {
 		return(
 			<div className="container-fluid text-center">
-				<h1 style={{...defaultStyle, "font-size": "54px"}}>						{this.state.serverData.user && 
-					this.state.serverData.user.name}'s projects
-				</h1>
+				{this.state.serverData.user	&&
+			<h1 style={{...defaultStyle, "font-size": "54px"}}>		
+				{this.state.serverData.user.name}'s projects
+			</h1>}
+				{this.state.serverData.user && 
+			<h2>		
+				{this.state.serverData.user.projects.length} Projects
+			</h2>}
 				<br/><br/><br/><br/>
 				
 				<div className="row">	
@@ -59,6 +66,11 @@ export default class Projects extends Component {
 					</div>
          
 					<h3>Project 3</h3>
+					<div className="col-md-5">
+						<p>Bear claw gingerbread pie cheesecake topping cookie chocolate cake. Tootsie roll carrot cake fruitcake marzipan cheesecake chocolate cake.Donut chocolate cake danish halvah fruitcake lollipop cake topping. Jelly-o bear claw pie sugar plum pudding chocolate bar gingerbread cotton candy pudding. Cake topping marshmallow. Powder gingerbread muffin sesame snaps cookie danish. Pudding candy croissant icing jujubes candy canes soufflé lemon drops biscuit. Macaroon sesame snaps tiramisu carrot cake. </p>
+					</div>
+
+					<h3>Project 4</h3>
 					<div className="col-md-5">
 						<p>Bear claw gingerbread pie cheesecake topping cookie chocolate cake. Tootsie roll carrot cake fruitcake marzipan cheesecake chocolate cake.Donut chocolate cake danish halvah fruitcake lollipop cake topping. Jelly-o bear claw pie sugar plum pudding chocolate bar gingerbread cotton candy pudding. Cake topping marshmallow. Powder gingerbread muffin sesame snaps cookie danish. Pudding candy croissant icing jujubes candy canes soufflé lemon drops biscuit. Macaroon sesame snaps tiramisu carrot cake. </p>
 					</div>
