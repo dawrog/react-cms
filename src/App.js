e63ab5058bbd37d653d4a024449426491991f4cd
dawrog <dawid.rogal@protonmail.com>
2018-01-03 20:17:01 +0100

    wrapped all projects data in ternary operator and changed logic

--- src/App.js
+++ src/App.js
@@ -2,59 +2,6 @@ import React, { Component } from "react"
 import "./App.css"
 import Header from "./components/Header"
 
-/*
-let defaultStyle = {
-  color: '#fff'
-};
-
-class Aggregate extends Component {
-  render() {
-    return (
-      <div style={{...defaultStyle, width: '40%', display: 'inline-block'}}>
-        <h2>Number Text</h2>
-      </div>
-    );
-  }
-}
-
-class Filter extends Component {
-  render() {
-    return (
-      <div style={defaultStyle}>
-        <img alt="filter"/>
-        <input type="text"/>
-      </div>
-    );
-  }
-}
-
-class Tiles extends Component {
-  render() {
-    return (
-      <div style={{...defaultStyle, display: 'inline-block', width: "25%"}}>
-      <img alt="pic1"/>
-      <h3>Tile Name</h3>
-      <ul><li>Work</li>Hobby<li></li><li>Contact</li></ul>
-      </div>
-    );
-  }
-} 
-
-class App extends Component {
-  render() {
-    return (
-    <div className="App" style={{'text-align': 'center'}}>
-        <h1 style={{...defaultStyle, 'font-size': '54px'}}>Title</h1>
-        <Header />
-        <Aggregate/>
-        <Aggregate/>
-        <Filter/>
-        <Tiles/>
-      </div>
-    );
-  }
-} */
-
 class App extends Component {
 	render() {
 		return (
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -44,37 +44,39 @@ export default class Projects extends Component {
 	render() {
 		return(
 			<div className="container-fluid text-center">
-				{this.state.serverData.user	&&
-			<h1 style={{...defaultStyle, "font-size": "54px"}}>		
-				{this.state.serverData.user.name}'s projects
-			</h1>}
-				{this.state.serverData.user && 
-			<h2>		
-				{this.state.serverData.user.projects.length} Projects
-			</h2>}
-				<br/><br/><br/><br/>
+				{this.state.serverData.user	?
+					<div>
+						<h1 style={{...defaultStyle, "font-size": "54px"}}>		
+							{this.state.serverData.user.name}'s projects
+						</h1>
+						<h2>		
+							{this.state.serverData.user.projects.length} Projects
+						</h2>
+						<br/><br/><br/><br/>
 				
-				<div className="row">	
-					<h3>Project 1</h3>
-					<div className="col-md-5">
-						<p>Bear claw gingerbread pie cheesecake topping cookie chocolate cake. Tootsie roll carrot cake fruitcake marzipan cheesecake chocolate cake.Donut chocolate cake danish halvah fruitcake lollipop cake topping. Jelly-o bear claw pie sugar plum pudding chocolate bar gingerbread cotton candy pudding. Cake topping marshmallow. Powder gingerbread muffin sesame snaps cookie danish. Pudding candy croissant icing jujubes candy canes soufflé lemon drops biscuit. Macaroon sesame snaps tiramisu carrot cake. </p>
-					</div>
+						<div className="row">	
+							<h3>Project 1</h3>
+							<div className="col-md-5">
+								<p>Bear claw gingerbread pie cheesecake topping cookie chocolate cake. Tootsie roll carrot cake fruitcake marzipan cheesecake chocolate cake.Donut chocolate cake danish halvah fruitcake lollipop cake topping. Jelly-o bear claw pie sugar plum pudding chocolate bar gingerbread cotton candy pudding. Cake topping marshmallow. Powder gingerbread muffin sesame snaps cookie danish. Pudding candy croissant icing jujubes candy canes soufflé lemon drops biscuit. Macaroon sesame snaps tiramisu carrot cake. </p>
+							</div>
 
-					<h3>Project 2</h3>
-					<div className="col-md-5">
-						<p>Bear claw gingerbread pie cheesecake topping cookie chocolate cake. Tootsie roll carrot cake fruitcake marzipan cheesecake chocolate cake.Donut chocolate cake danish halvah fruitcake lollipop cake topping. Jelly-o bear claw pie sugar plum pudding chocolate bar gingerbread cotton candy pudding. Cake topping marshmallow. Powder gingerbread muffin sesame snaps cookie danish. Pudding candy croissant icing jujubes candy canes soufflé lemon drops biscuit. Macaroon sesame snaps tiramisu carrot cake. </p>
-					</div>
+							<h3>Project 2</h3>
+							<div className="col-md-5">
+								<p>Bear claw gingerbread pie cheesecake topping cookie chocolate cake. Tootsie roll carrot cake fruitcake marzipan cheesecake chocolate cake.Donut chocolate cake danish halvah fruitcake lollipop cake topping. Jelly-o bear claw pie sugar plum pudding chocolate bar gingerbread cotton candy pudding. Cake topping marshmallow. Powder gingerbread muffin sesame snaps cookie danish. Pudding candy croissant icing jujubes candy canes soufflé lemon drops biscuit. Macaroon sesame snaps tiramisu carrot cake. </p>
+							</div>
          
-					<h3>Project 3</h3>
-					<div className="col-md-5">
-						<p>Bear claw gingerbread pie cheesecake topping cookie chocolate cake. Tootsie roll carrot cake fruitcake marzipan cheesecake chocolate cake.Donut chocolate cake danish halvah fruitcake lollipop cake topping. Jelly-o bear claw pie sugar plum pudding chocolate bar gingerbread cotton candy pudding. Cake topping marshmallow. Powder gingerbread muffin sesame snaps cookie danish. Pudding candy croissant icing jujubes candy canes soufflé lemon drops biscuit. Macaroon sesame snaps tiramisu carrot cake. </p>
-					</div>
+							<h3>Project 3</h3>
+							<div className="col-md-5">
+								<p>Bear claw gingerbread pie cheesecake topping cookie chocolate cake. Tootsie roll carrot cake fruitcake marzipan cheesecake chocolate cake.Donut chocolate cake danish halvah fruitcake lollipop cake topping. Jelly-o bear claw pie sugar plum pudding chocolate bar gingerbread cotton candy pudding. Cake topping marshmallow. Powder gingerbread muffin sesame snaps cookie danish. Pudding candy croissant icing jujubes candy canes soufflé lemon drops biscuit. Macaroon sesame snaps tiramisu carrot cake. </p>
+							</div>
 
-					<h3>Project 4</h3>
-					<div className="col-md-5">
-						<p>Bear claw gingerbread pie cheesecake topping cookie chocolate cake. Tootsie roll carrot cake fruitcake marzipan cheesecake chocolate cake.Donut chocolate cake danish halvah fruitcake lollipop cake topping. Jelly-o bear claw pie sugar plum pudding chocolate bar gingerbread cotton candy pudding. Cake topping marshmallow. Powder gingerbread muffin sesame snaps cookie danish. Pudding candy croissant icing jujubes candy canes soufflé lemon drops biscuit. Macaroon sesame snaps tiramisu carrot cake. </p>
-					</div>
-				</div>
+							<h3>Project 4</h3>
+							<div className="col-md-5">
+								<p>Bear claw gingerbread pie cheesecake topping cookie chocolate cake. Tootsie roll carrot cake fruitcake marzipan cheesecake chocolate cake.Donut chocolate cake danish halvah fruitcake lollipop cake topping. Jelly-o bear claw pie sugar plum pudding chocolate bar gingerbread cotton candy pudding. Cake topping marshmallow. Powder gingerbread muffin sesame snaps cookie danish. Pudding candy croissant icing jujubes candy canes soufflé lemon drops biscuit. Macaroon sesame snaps tiramisu carrot cake. </p>
+							</div>
+						</div>
+					</div> : "Loading..."
+				}
 			</div>
 		)
 	}
