143521125ad786146bea5ad813f550db21425403
dawrog <dawid.rogal@protonmail.com>
2018-01-18 23:39:48 +0100

    changed components to functional

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,134 +1,124 @@
-import React, { Component } from "react"
-import "../App.css"
+import React, { Component } from 'react';
+import '../App.css';
 
 let defaultStyle = {
-	color: "#fff"
-}
+	color: '#fff'
+};
 
 let fakeServerData = {
 	user: {
-		name: "User",
+		name: 'User',
 		projects: [
 			{
-				name: "Wordpress Site",
-				technologies: ["HTML", "PHP", "CSS", "jQuery"],
+				name: 'Wordpress Site',
+				technologies: ['HTML', 'PHP', 'CSS', 'jQuery'],
 				days: [
-					{deadline: "11 listopada 2018", duration: 93},
+					{deadline: '11 listopada 2018', duration: 93},
 				]
 			},
 			{
-				name: "Weather App",
-				technologies: ["JS", "HTML", "CSS"],
+				name: 'Weather App',
+				technologies: ['JS', 'HTML', 'CSS'],
 				days: [
-					{deadline: "11 listopada 2018", duration: 69},
+					{deadline: '11 listopada 2018', duration: 69},
 				]
 			},
 			{
-				name: "React CMS",
-				technologies: ["JS", "React", "HTML", "Bootstrap"],
+				name: 'React CMS',
+				technologies: ['JS', 'React', 'HTML', 'Bootstrap'],
 				days: [
-					{deadline: "11 listopada 2018", duration: 88},
+					{deadline: '11 listopada 2018', duration: 88},
 				]
 			},
 			{
-				name: "HTML5 Game",
-				technologies: ["JS", "HTML", "CSS"],
+				name: 'HTML5 Game',
+				technologies: ['JS', 'HTML', 'CSS'],
 				days: [
-					{deadline: "11 listopada 2018", duration: 23},
+					{deadline: '11 listopada 2018', duration: 23},
 				]
 			}
 		]
 	}
 
+};
+
+function Aggregate( {projects} ) { 
+	return (
+		<div style={{...defaultStyle, margin: '2%'}}>
+			<h2>{projects.length} Pending Projects</h2>
+		</div>
+	);
 }
 
-class Aggregate extends Component {
-	render() {
-		return (
-			<div style={{...defaultStyle, margin: "2%"}}>
-				<h2>{this.props.projects.length} Pending Projects</h2>
-			</div>
-		)
-	}
-} 
-
-class Filter extends Component {
-	render() {
-		return (
-			<div style={defaultStyle}>
-				<img/>
-				<input type="text" onKeyUp={event => this.props.onTextChange(event.target.value)}/>
-			</div>
-		)	
-	}
+function Filter( {onTextChange} ) {
+	return (
+		<div style={defaultStyle}>
+			<img/>
+			<input type="text" onKeyUp={event => onTextChange(event.target.value)}/>
+		</div>
+	);	
 }
 
-class ProjectGenerator extends Component {
-	render() {
-		return (
-			<div>
-				<h3>{this.props.project.name}</h3>
-
-				<p>Bear claw gingerbread pie cheesecake topping cookie chocolate cake. Tootsie roll carrot cake fruitcake marzipan cheesecake chocolate cake.Donut chocolate cake danish halvah fruitcake lollipop cake topping. Jelly-o bear claw pie sugar plum pudding chocolate bar gingerbread cotton candy pudding. Cake topping marshmallow. Powder gingerbread muffin sesame snaps cookie danish. Pudding candy croissant icing jujubes candy canes soufflé lemon drops biscuit. Macaroon sesame snaps tiramisu carrot cake. </p>
-			</div>
+function ProjectGenerator( {project} ) {
+	return (
+		<div>
+			<h3>{project.name}</h3>
+			<p>Bear claw gingerbread pie cheesecake topping cookie chocolate cake. Tootsie roll carrot cake fruitcake marzipan cheesecake chocolate cake.Donut chocolate cake danish halvah fruitcake lollipop cake topping. Jelly-o bear claw pie sugar plum pudding chocolate bar gingerbread cotton candy pudding. Cake topping marshmallow. Powder gingerbread muffin sesame snaps cookie danish. Pudding candy croissant icing jujubes candy canes soufflé lemon drops biscuit. Macaroon sesame snaps tiramisu carrot cake. </p>
+		</div>
 			
-		)
-	}
+	);
 } 
 
-class DaysCounter extends Component {
-	render() {
-		let projects = this.props.projects
-		let allDays = projects.reduce((days, eachProject) => {
-			return days.concat(eachProject.days)
-		}, [])
-		let totalDuration = allDays.reduce((sum, eachDay) => {
-			return sum + eachDay.duration
-		}, 0)
-		return (
-			<div>
-				<h2>{Math.round(totalDuration/60)} Days</h2>
-				<ul>{projects.map(deadline =>
-					<li>{deadline.name}</li>
-				)}
-				</ul>
-			</div>
+function DaysCounter( {projects} ) {
+	let allDays = projects.reduce((days, eachProject) => {
+		return days.concat(eachProject.days);
+	}, []);
+	let totalDuration = allDays.reduce((sum, eachDay) => {
+		return sum + eachDay.duration;
+	}, 0);
+	return (
+		<div>
+			<h2>{Math.round(totalDuration/60)} Days</h2>
+			<ul>{projects.map(deadline =>
+				<li>{deadline.name}</li>
+			)}
+			</ul>
+		</div>
 		
-		) 
-	}
+	); 
 } 
 
 export default class Projects extends Component {
 	constructor() {
-		super()
+		super();
 		this.state = {
 			serverData: {},
-			filterString: ""
-		}
+			filterString: ''
+		};
 	}
 
 	componentDidMount() {
 		setTimeout(() =>{
-			this.setState({serverData: fakeServerData})
-		}, 1000)
+			this.setState({serverData: fakeServerData});
+		}, 1000);
 	}
 
 	render() {
 		let playlistToRender = this.state.serverData.user ?this.state.serverData.user.projects.filter(project =>
 			project.name.toLowerCase().includes(
 				this.state.filterString.toLowerCase())
-		) : []
+		) : [];
 		return	(
-			<div className="container-fluid text-center" style={{"margin": "1%"}}>
+			<div className="container-fluid text-center" style={{'margin': '1%'}}>
 				{this.state.serverData.user	?
 					<div>
-						<h1 style={{...defaultStyle, "font-size": "54px"}}>		
+						<h1 style={{...defaultStyle, 'font-size': '54px'}}>		
 							{this.state.serverData.user.name}'s projects
 						</h1>
 						<Aggregate projects={playlistToRender}/>
 						<Filter onTextChange={text => this.setState({filterString: text})}/>
 						{playlistToRender.map(project =>
-							<div style={{"width": "40%","float": "left", "margin": "5%", "text-align": "justify"}}>
+							<div style={{'width': '40%','float': 'left', 'margin': '5%', 'text-align': 'justify'}}>
 								<ProjectGenerator project={project}/>
 								<div className="container-fluid text-center">
 									<DaysCounter projects={playlistToRender}/>
@@ -136,10 +126,10 @@ export default class Projects extends Component {
 							</div>
 						)}
 
-					</div> : "Loading..."
+					</div> : 'Loading...'
 				}
 			</div>
-		)
+		);
 	}
 }
 
